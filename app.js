223fec6677813b62136494cac84567f4a56d7034
VishwaAmarasinghe <90438593+VishwaAmarasinghe@users.noreply.github.com>
2021-12-10 10:37:53 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var sections = $('section')
   , nav = $('nav')
-  , nav_height = nav.outerHeight();
+  , nav_height = nav.outerHeight()+1;
 
 $(window).on('scroll', function () {
   var cur_pos = $(this).scrollTop();
@@ -25,11 +25,57 @@ nav.find('a').on('click', function () {
     , id = $el.attr('href');
   
   $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
+    scrollTop: $(id).offset().top - nav_height+1
   }, 500);
   
   return false;
 });
+// Cache selectors
+var lastId,
+ topMenu = $("#mainNav"),
+ topMenuHeight = topMenu.outerHeight()+1,
+ // All list items
+ menuItems = topMenu.find("a"),
+ // Anchors corresponding to menu items
+ scrollItems = menuItems.map(function(){
+   var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+ });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 850);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
 
 /*my works*/
 
